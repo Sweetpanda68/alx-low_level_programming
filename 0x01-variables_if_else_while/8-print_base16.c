ffbca24826952ea60017b48e081e5e87dd7d4e0f
Sweetpanda68 <mgirma976@gmail.com>
2022-12-09 06:23:47 -0800

    c var

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -6,13 +6,13 @@
  */
 int main(void)
 {
-		int i;
-			char c;
+int i;
+char c;
 
-				for (i = 0; i < 10; i++)
-							putchar(i % 10 + '0');
-					for (c = 'a'; c < 'g'; c++)
-								putchar(c);
+for (i = 0; i < 10; i++)
+putchar(i % 10 + '0');
+for (c = 'a'; c < 'g'; c++)
+putchar(c);
 putchar('\n');
 return (0);
 }
