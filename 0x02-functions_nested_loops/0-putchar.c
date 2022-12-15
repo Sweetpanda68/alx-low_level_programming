b281722893fa55cfa307a020e4b63b7b9b6cdcc3
Sweetpanda68 <mgirma976@gmail.com>
2022-12-14 18:51:29 -0800

    fnl

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,9 +1,12 @@
 #include "main.h"
+
 /**
  * main - entry block
  * @void: no argument
- * Return: 0
+ * Return: return 0
+ *
  **/
+
 int main(void)
 {
 char _ermias[] = "_putchar";
@@ -12,8 +15,8 @@ int c;
 
 for (c=0; c < 8; c++)
 
-_putchar(ermias[c]);
-_putchar('\n');
+_putchar(_ermias[c]);
+_putchar("\n");
 
 return (0);
 }
