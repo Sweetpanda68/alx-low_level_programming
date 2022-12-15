dbc18daad8ecc0e91c140d8366dc68a00793ac31
Sweetpanda68 <mgirma976@gmail.com>
2022-12-15 14:02:26 -0800

    4fnl

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -6,8 +6,6 @@
  */
 void print_diagonal(int n)
 {
-int i;
-int j;
 
 if (n <= 0)
 {
@@ -15,13 +13,17 @@ _putchar('\n');
 } 
 else
 {
+int i, j
+
 for (i = 0; i < n; i++)
 {
-for (j = 0; j < i; j++)
+for (j = 0; j < n; j++)
 {
+if (j == 1)
+_putchar('\\');
+else if (j < i)
 _putchar(' ');
 }
-_putchar('\\');
 _putchar('\n');
 }
 }
