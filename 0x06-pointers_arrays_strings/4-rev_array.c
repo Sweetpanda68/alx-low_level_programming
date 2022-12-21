0c1f6f6f7263c0c48ad28b5d52dabd8bfe98e4a5
Sweetpanda68 <mgirma976@gmail.com>
2022-12-21 11:35:01 -0800

    6pas

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -10,9 +10,10 @@ void reverse_array(int *a, int n)
 	int tmp, index;
 
 	for (index = n - 1; index >= n / 2; index--)
+	{
 
 		tmp = a[n - 1 - index];
 	a[n - 1 - index] = a[index];
 	a[index] = tmp;
-}
+	}
 }
