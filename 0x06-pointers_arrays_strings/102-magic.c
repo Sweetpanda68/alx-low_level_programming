55ab052167acd7c3c03c7c0368dface1f0646524
Sweetpanda68 <mgirma976@gmail.com>
2022-12-23 10:57:52 -0800

    6pas

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -16,8 +16,8 @@ a[2] = 1024;
 		     * - only one statement
 		     * - you are not allowed to code anything else than this line of code
 		     */
-			   *(p + 5) = 98;
-		      /* ...so that this prints 98\n */
-		      printf("a[2] = %d\n", a[2]);
+		   *(p + 5) = 98;
+	      /* ...so that this prints 98\n */
+     		  printf("a[2] = %d\n", a[2]);
 return (0);
 }
