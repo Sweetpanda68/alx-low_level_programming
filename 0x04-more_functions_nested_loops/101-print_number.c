e2146a1eb42c56e0cdeb459e535f1bcbbcadb03e
Sweetpanda68 <mgirma976@gmail.com>
2023-01-02 12:00:38 -0800

    4fnl

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,25 +1,46 @@
-#include <stdio.h>
-/**
- * main - main function
- *
- * Return: returns 0
- */
-int main(void)
-{
-unsigned int a = 2;
-unsigned long n = 612852475143;
+#include "main.h"
 
-while (a != n)
+/**
+ * print_number - prints number
+ *
+ * @n: integer to print to character
+ */
+void print_number(int n)
 {
-if (n % a == 0)
-{
-n = n / a;
-}
-else
-{
-a++;
-}
-}
-printf("%lu\n", n);
-return (0);
+	int i;
+	int d = 1;
+	unsigned int x = n;
+	unsigned int y = n;
+	int c = 0;
+
+	if (n == 0)
+	{
+		_putchar('0');
+	}
+	if (n < 0)
+	{
+		_putchar('-');
+		n = n + 1;
+		n = -n;
+		y = n;
+		x = n;
+		x += 1;
+		y += 1;
+	}
+
+	while (x != 0)
+	{
+		x = x / 10;
+		c++;
+	}
+	for (i = 1; i < c; i++)
+	{
+		d *= 10;
+	}
+	for (i = 0; i < c; i++)
+	{
+		_putchar(y / d + '0');
+		y = y % d;
+		d = d / 10;
+	}
 }
